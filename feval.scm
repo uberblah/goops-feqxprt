fd0cc48072a06963d5b051d7dedad414cd402b4a
Dakota Fisher <dafi7849@colorado.edu>
2015-12-28 22:11:34 +0000

    quote-env and evaluation combinator notes

--- feval.scm
+++ feval.scm
@@ -12,7 +12,7 @@
 
 (define-box <proc-env>)
 					;(define-unit <lit-env> lit-env)
-(define lit-env (make-box <proc-env> identity))
+					;(define lit-env (make-box <proc-env> identity))
 					;(define-unit <lisp-base-env>)
 (define lisp-base-env
   (make-box <proc-env>
@@ -37,7 +37,7 @@
   (if (pair? l)
       (apply f (map (cut feval <> env) (cdr l)))
       f))
-(define-method (fapply f l env) l)
+					;(define-method (fapply f l env) l)
 
 (define-syntax procs->env
   (syntax-rules ()
@@ -48,66 +48,73 @@
 
 (define r5rs-proc-env
   (procs->env ; equivalence
-              eqv? eq? equal?
-	      ; numbers
-	      number? complex? real? rational? integer? exact? inexact?
-	      = < > <= >= zero? positive? negative? odd? even?
-	      + - * quotient remainder modulo max min abs numerator denominator
-	      gcd lcm floor ceiling truncate round rationalize expt
-	      / exp log sin cos tan asin acos atan sqrt
-	      make-rectangular make-polar real-part imag-part magnitude angle
-	      exact->inexact inexact->exact
-	      number->string string->number
-	      ; booleans
-	      not boolean?
-	      ; pairs and lists
-	      pair? cons car cdr set-car!
-	      caar cadr cdar cddr caaar caadr cadar caddr cdaar cdadr cddar cdddr
-	      caaaar caaadr caadar caaddr cadaar cadadr caddar cadddr cdaaar
-	      cdaadr cdadar cdaddr cddaar cddadr cdddar cddddr
-	      null? list? list length append reverse list-tail list-ref
-	      memq memv member assq assv assoc
-	      ; symbols
-              symbol? symbol->string string->symbol
-	      ; characters
-	      char? char=? char<? char>? char<=? char>=? char-ci=? char-ci<?
-	      char-ci<=? char-ci>=? char-alphabetic? char-numeric?
-	      char-whitespace? char-upper-case? char-lower-case? char->integer
-	      integer->char char-upcase char-downcase
-	      ; strings
-	      string? make-string string string-length string-ref string-set!
-	      string=? string>? string>? string<=? string>=? string-ci<?
-	      string-ci>? string-ci<=? string-ci>=? substring string-append
-	      string->list list->string string-copy string-fill!
-	      ; vectors
-	      vector? make-vector vector vector-length vector-ref vector-set!
-	      vector->list list->vector vector-fill!
-	      ; control features
-	      procedure? apply map for-each force call-with-current-continuation
-	      values call-with-values dynamic-wind
-	      ; eval
-	      eval #;scheme-report-environment #;null-environment
-	      interaction-environment
-	      ; ports
-	      call-with-input-file call-with-output-file input-port?
-	      output-port? current-input-port current-output-port
-	      with-input-from-file with-output-to-file open-input-file
-	      open-output-file close-input-port close-output-port
-	      ; input
-	      read read-char peek-char eof-object? char-ready?
-	      ; output
-	      write display newline write-char
-	      ; system interface
-	      load #;transcript-on #;transcript-off))
+   eqv? eq? equal?
+					; numbers
+   number? complex? real? rational? integer? exact? inexact?
+   = < > <= >= zero? positive? negative? odd? even?
+   + - * quotient remainder modulo max min abs numerator denominator
+   gcd lcm floor ceiling truncate round rationalize expt
+   / exp log sin cos tan asin acos atan sqrt
+   make-rectangular make-polar real-part imag-part magnitude angle
+   exact->inexact inexact->exact
+   number->string string->number
+					; booleans
+   not boolean?
+					; pairs and lists
+   pair? cons car cdr set-car!
+   caar cadr cdar cddr caaar caadr cadar caddr cdaar cdadr cddar cdddr
+   caaaar caaadr caadar caaddr cadaar cadadr caddar cadddr cdaaar
+   cdaadr cdadar cdaddr cddaar cddadr cdddar cddddr
+   null? list? list length append reverse list-tail list-ref
+   memq memv member assq assv assoc
+					; symbols
+   symbol? symbol->string string->symbol
+					; characters
+   char? char=? char<? char>? char<=? char>=? char-ci=? char-ci<?
+   char-ci<=? char-ci>=? char-alphabetic? char-numeric?
+   char-whitespace? char-upper-case? char-lower-case? char->integer
+   integer->char char-upcase char-downcase
+					; strings
+   string? make-string string string-length string-ref string-set!
+   string=? string>? string>? string<=? string>=? string-ci<?
+   string-ci>? string-ci<=? string-ci>=? substring string-append
+   string->list list->string string-copy string-fill!
+					; vectors
+   vector? make-vector vector vector-length vector-ref vector-set!
+   vector->list list->vector vector-fill!
+					; control features
+   procedure? apply map for-each force call-with-current-continuation
+   values call-with-values dynamic-wind
+					; eval
+   eval #;scheme-report-environment #;null-environment
+   interaction-environment
+					; ports
+   call-with-input-file call-with-output-file input-port?
+   output-port? current-input-port current-output-port
+   with-input-from-file with-output-to-file open-input-file
+   open-output-file close-input-port close-output-port
+					; input
+   read read-char peek-char eof-object? char-ready?
+					; output
+   write display newline write-char
+					; system interface
+   load #;transcript-on #;transcript-off))
 
 (define r5rs-base-env (append r5rs-proc-env lisp-base-env))
 
 (define-fstruct <fprocedure> code env)
 (define-method (fapply (proc <fprocedure>) expr env)
   (match (fstruct->list proc)
-    (((_ expr-arg env-arg . body) inner-env)
+    (((_ expr-arg env-arg body) inner-env)
      #;(feval (zip-args expr-args body) (acons env-arg env inner-env))
      (feval body (acons expr-arg expr (acons env-arg env inner-env))))))
+
+(define quote-env
+  (make-box <proc-env>
+	    (lambda (x)
+	      (make-fstruct <fprocedure>
+			    '(flambda expr env expr)
+			    lit-env))))
 
 (define (make-closure args body env)
   (define (zip-args args in-expr env)
@@ -149,7 +156,7 @@
 	     (else `(letrec ,(reverse defs) (begin . ,body)))))))
   (rec '() body))
 
-; derived expressions: cond case and or let let* letrec begin do delay quasiquote
+					; derived expressions: cond case and or let let* letrec begin do delay quasiquote
 (define (syntax-rules-proc expr env body)
   (define (match-pattern pattern expr lits)
     (cond ((member pattern lits) (and (equal? expr lits) '()))
@@ -184,7 +191,7 @@
 (feval '(+ 2 2) `((+ . ,+) . ,lisp-base-env))
 (feval '(+ 2 2) lisp-base-env) ; this is supposed to yield an error
 (feval '(eval (list (string->symbol "+") 2 2) (scheme-report-environment 5)) r5rs-base-env)
-(feval '(+ 2 2) lit-env)
+(feval '(+ 2 2) quote-env)
 ((make-closure 'x '(apply + x) r5rs-base-env) 1 2 3 4 5)
 (r5rs-transform-body '((begin (define a b)) (define (b) c) expr1 (expr2)))
 (r5rs-transform-body '(expr))
@@ -195,4 +202,5 @@
 (define-syntax-rule (alist-alist beg (var val ...) ...) (list (list beg var val ...) ...))
 (alist-alist 0 (1 2 3) (4 5 6) (7 8 9))
 
+(feval `(,+ (2) (() 2)) quote-env)
 |#
